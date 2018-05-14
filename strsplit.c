ccb4752fadfb9d9541a91da5ff7eb147301743de
Antoni Sawicki <as@tenoware.com>
2018-05-13 22:44:07 -0700

    update

--- strsplit.c
+++ strsplit.c
@@ -8,81 +8,82 @@
 // Written by Tomasz Nowak
 //
 int strsplit(char *src, char ***dst, char *sep) {
-	char **arr;
+    char **arr;
     char defsep[]=" \t\n\r\f";
 
     if(!sep)
         sep=defsep;
 
-	char *src_org = src;
-	char *c;
-	int n = 0;
+    char *src_org = src;
+    char *c;
+    int n = 0;
 
-	while (c = strpbrk(src, sep))
-	{
-		while (c == src)
-		{
-			src++;
-			c = strpbrk(src, sep);
-		}
-		if (c == NULL) break;
+    while (c = strpbrk(src, sep))
+    {
+        while (c == src)
+        {
+            src++;
+            c = strpbrk(src, sep);
+        }
+        if (c == NULL) break;
 
-		src = c + 1;
-		n++;
-	}
+        src = c + 1;
+        n++;
+    }
 
-	int nelem = n + 1;
-	arr = (char **)malloc(sizeof(char *) * nelem);
-	memset(arr, 0, sizeof(char *) * nelem);
+    int nelem = n + 1;
+    arr = (char **)malloc(sizeof(char *) * nelem);
+    memset(arr, 0, sizeof(char *) * nelem);
 
-	src = src_org;
-	n = 0;
+    src = src_org;
+    n = 0;
 
-	while (c = strpbrk(src, sep))
-	{
-		while (c == src)
-		{
-			src++;
-			c = strpbrk(src, sep);
-		}
-		if (c == NULL) break;
+    while (c = strpbrk(src, sep))
+    {
+        while (c == src)
+        {
+            src++;
+            c = strpbrk(src, sep);
+        }
+        if (c == NULL) break;
 
-		*c = 0;
-		arr[n] = src;
+        *c = 0;
+        arr[n] = src;
 
-		src = c + 1;
-		n++;
-	}
+        src = c + 1;
+        n++;
+    }
 
-	arr[n] = src;
+    arr[n] = src;
 
-	*dst = arr;
-	return nelem;
+    *dst = arr;
+    return nelem;
 }
 
 int main(int argc, char **argv) {
-	char **arr;
-	int len, n;
-	char tmp[256]={0};
+    char **arr;
+    int len, n;
+    char tmp[256]={0};
 
-	strcpy(tmp, "The quick       brown\tfox\t\t\t\tjumps\t \t \t \t \t over\r\nthe\n\n\n\nlazy\fdog\f \f\f");
+    // example 1 - default separator
+    strcpy(tmp, "The quick       brown\tfox\t\t\t\tjumps\t \t \t \t \t over\r\nthe\n\n\n\nlazy\fdog\f \f\f");
 
     len = strsplit(tmp, &arr, 0);
 
-	for (n = 0; n<len; n++)
-		printf("%s\n", arr[n]);
+    for (n = 0; n<len; n++)
+        printf("%s\n", arr[n]);
 
-	free(arr);
+    free(arr);
 
-
-	strcpy(tmp, "/var/lib///my/./broken/.../app/././././path///");
+    // example 2 - custom separators
+    strcpy(tmp, "/var/lib///my/./broken/.../app/././././path///");
 
     len = strsplit(tmp, &arr, "/.");
 
-	for (n = 0; n<len; n++)
-		printf("%s\n", arr[n]);
+    for (n = 0; n<len; n++)
+        printf("%s\n", arr[n]);
 
-	free(arr);
+    free(arr);
 
-	return 0;
+    return 0;
 }
